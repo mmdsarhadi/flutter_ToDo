b575719a678bd78b92af9d8c3d9e6a188a32c8f5
mmdsarhadi <mmdsarhadi@gmail.com>
2024-01-22 19:15:07 +0330

    Completing the English version program and signing it for publication

--- android/app/src/main/kotlin/com/todolist/software/MainActivity.kt
+++ android/app/src/main/kotlin/com/todolist/software/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.todolist
+package com.todolist.software
 
 import io.flutter.embedding.android.FlutterActivity
 
--- lib/main.dart
+++ lib/main.dart
@@ -62,7 +62,9 @@ class MyApp extends StatelessWidget {
       locale: Locale('en'),
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      theme: ThemeData(
+      theme:
+      ThemeData(
+
           textTheme: getPrimaryTextTheme(),
           inputDecorationTheme: const InputDecorationTheme(
               floatingLabelBehavior: FloatingLabelBehavior.never,
@@ -89,6 +91,97 @@ class MyApp extends StatelessWidget {
 
 
 }
+
+
+
+
+
+class MyAppThemeConfig {
+  static const String faPrimaryFontFamily = 'IranYekan';
+  final Color primaryColor = Colors.pink.shade400;
+  final Color primaryTextColor;
+  final Color secondaryTextColor;
+  final Color surfaceColor;
+  final Color backgroundColor;
+  final Color appBarColor;
+  final Brightness brightness;
+
+  MyAppThemeConfig.dark()
+      : primaryTextColor = Colors.white,
+        secondaryTextColor = Colors.white70,
+        surfaceColor = Color(0x0dffffff),
+        backgroundColor = Color.fromARGB(255, 30, 30, 30),
+        appBarColor = Colors.black,
+        brightness = Brightness.dark;
+
+  MyAppThemeConfig.light()
+      : primaryTextColor = Colors.grey.shade900,
+        secondaryTextColor = Colors.grey.shade900.withOpacity(0.8),
+        surfaceColor = Color(0x0d000000),
+        backgroundColor = Colors.white,
+        appBarColor = Color.fromARGB(255, 235, 235, 235),
+        brightness = Brightness.light;
+
+  ThemeData getTheme(String languageCode) {
+    return ThemeData(
+
+      primarySwatch: Colors.pink,
+      primaryColor: primaryColor,
+      brightness: brightness,
+
+      dividerColor: surfaceColor,
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all(primaryColor),
+        ),
+      ),
+      scaffoldBackgroundColor: backgroundColor,
+      appBarTheme: AppBarTheme(
+          elevation: 0,
+          backgroundColor: appBarColor,
+          foregroundColor: primaryTextColor),
+      inputDecorationTheme: InputDecorationTheme(
+        labelStyle: TextStyle(fontSize: 14, fontWeight: FontWeight.normal),
+        border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(8),
+            borderSide: BorderSide.none),
+        filled: true,
+        fillColor: surfaceColor,
+      ),
+      textTheme: languageCode == 'en' ? enPrimaryTextTheme : faPrimaryTextTheme,
+    );
+  }
+
+  TextTheme get enPrimaryTextTheme => GoogleFonts.latoTextTheme(TextTheme(
+    bodyText2: TextStyle(fontSize: 15, color: primaryTextColor),
+    bodyText1: TextStyle(fontSize: 13, color: secondaryTextColor),
+    headline6:
+    TextStyle(fontWeight: FontWeight.bold, color: primaryTextColor),
+    subtitle1: TextStyle(
+        fontSize: 16, fontWeight: FontWeight.bold, color: primaryTextColor),
+  ));
+
+  TextTheme get faPrimaryTextTheme => TextTheme(
+
+      headline6: TextStyle(
+          fontSize: 18,
+          fontWeight: FontWeight.bold,
+          color: primaryTextColor,
+          fontFamily: faPrimaryFontFamily),
+
+      button: TextStyle(fontFamily: faPrimaryFontFamily)
+
+  );
+}
+
+
+
+
+
+
+
+
+
 
 class HomeScreen extends StatelessWidget {
   HomeScreen({Key? key}) : super(key: key);
